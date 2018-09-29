1849abd334b1ef4f998a88a568dc7140ea3bd14c
adel2306 <dadelina@purdue.edu>
2018-09-29 12:45:29 -0400

    Changed the name of first and last name

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -5,7 +5,7 @@ public class MathUtils {
      */
     public static double divideNumbers(int dividend, int divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
+        double res = ((double)dividend) / divisor;
         /* Return the DEFINITELY CORRECT result */
         return res;
     }
@@ -21,20 +21,19 @@ public class MathUtils {
         /* Determine if the exponent is negative */
         boolean isNegative = false;
         if (exp < 0) {
-            exp *= -1;
             isNegative = true;
         }
 
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
-        for (int i = 0; i < exp; i++) {
-           res *= base; 
-        }
+        for (int i = 1; i <= exp; i++) {
+           res= (int)Math.pow(base,exp);
+           }
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
+            double dividedRes = Math.pow(base,exp);
             return dividedRes;
         }
         /* Otherwise, we are safe to just return the result */
